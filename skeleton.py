e192237fca794571f622bda1c8b61ba19f3732ca
Linnéa Löfdahl <linnea@nekomata.se>
2013-11-28 20:27:40 +0100

    Download images from wikipedia and create sprites from them

--- skeleton.py
+++ skeleton.py
@@ -1,6 +1,10 @@
 import pygame
+
 from sprite import *
 from text import *
+
+WHITE = (250, 250, 250)
+BLACK = (0, 0, 0)
 
 def main():
   # Initialize pygame and setup the window 
@@ -9,17 +13,28 @@ def main():
 
   # Set a white background, same size as window
   background = pygame.Surface(screen.get_size())
-  background.fill((250, 250, 250))
+  background.fill(WHITE)
 
   # Create a text object at position 10, 10
   text = Text('Pyladies', (10, 10))
 
-  # Create a sprite and add it to a list of sprites to render
+  # Create a sprite from a local image 
   kitten = Sprite('kitten.jpg')
-  allsprites = pygame.sprite.RenderPlain((kitten))
 
+  # Download an image from wikipedia and create a sprite from it 
+  tom_hanks = WikipediaSprite('Tom Hanks')
+
+  # Create a group with the sprites to draw together
+  allsprites = pygame.sprite.LayeredUpdates((tom_hanks, kitten))
+
+  clock = pygame.time.Clock()
   running = True
+
   while running:
+
+    # Limit the while loop to a max of 20 times per second.
+    clock.tick(20)
+
     # Handle input events from the user
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -27,9 +42,14 @@ def main():
 
     kitten.update()
 
+    # draw background and text
     screen.blit(background, (0, 0))
     screen.blit(text.surface, text.position)
 
+    # Draw a solid rectangle
+    pygame.draw.rect(screen, BLACK, [0, 460, 180, 40])
+
+    # render sprites and update screen
     allsprites.draw(screen) 
     pygame.display.update()
 
--- sprite.py
+++ sprite.py
@@ -1,14 +1,14 @@
-import pygame, os
+import pygame, os, wikiapi, urllib
 
 class Sprite(pygame.sprite.Sprite):
-  def __init__(self, imageName):
+  def __init__(self, image_name):
     pygame.sprite.Sprite.__init__(self) #call Sprite intializer
-    self.image = self.load_image(imageName)
+    self.image = self.load_image(image_name)
     self.rect = self.image.get_rect()
     self.rect.center = (250, 250)
 
-  def load_image(self, imageName):
-    fullPath = os.path.join("images", imageName)
+  def load_image(self, image_name):
+    fullPath = os.path.join("images", image_name)
 
     try:
       image = pygame.image.load(fullPath)
@@ -25,3 +25,16 @@ class Sprite(pygame.sprite.Sprite):
 
   def update(self):
     self.rect.topleft = pygame.mouse.get_pos()
+
+class WikipediaSprite(Sprite):
+  def __init__(self, article_name):
+    self.article_name = article_name
+    self.image_name = self.download_wikipedia_image(article_name)
+    Sprite.__init__(self, self.image_name)
+
+  def download_wikipedia_image(self, name):
+    wikiapi.WikiApi().get_article(name)
+    image_url = wikiapi.WikiApi().get_article(name).image
+    image_name = name + '.jpg'
+    urllib.URLopener().retrieve(image_url, 'images/' + image_name)
+    return image_name
