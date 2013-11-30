66a645e4386efcc4c6c5e6c13dfddf51845393cb
Oxana <merenlin@gmail.com>
2013-11-30 14:57:27 +0100

    Prettyfing the shelve example

--- data_processor.py
+++ data_processor.py
@@ -1,23 +1,31 @@
 from imdb import imdb
 import shelve
 import random 
+import os
 
 actors = list()
 
 def create_entry(actor, date, title, role):
     # create a dictionary for each row in imdb file
-    entry = {'actor':str(actor),'title':title,'data':date,'role':role}
+    entry = {'actor': actor,'title': title,'data': date,'role': role}
     # append this dictionary to our list
     actors.append(entry)
 
 # call create_entry for every row in imdb file
 imdb.process_file('data/actors.list.gz', create_entry)
 
-# create a new shelve 
+# remove the database if already exists
+try:
+    os.remove('data/actors.db')
+except OSError:
+    pass
+
+# create a new shelve database
 s = shelve.open('data/actors.db')
 
-# pick 15 random entries from the imdb data
-indexes_to_save = random.sample(range(0,len(actors)-1), 15)
+# pick only n random entries from the imdb data
+n = 15
+indexes_to_save = random.sample(range(0,len(actors)-1), n)
 
 # save these picked entries to the database
 try:
